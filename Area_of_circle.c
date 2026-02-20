8f20bc9915ce86ec25de7f1728ae7d68260e065e
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-02-20 20:05:55 +0530

    Add files via upload

---
+++ Area_of_circle.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+  float r,pi=3.1415;
+  printf("enter radius:");
+  scanf("%f",&r);             // taking radius from user
+  printf("area of circle is %f square unit",3.14*(r*r));
+  return 0;
+}
