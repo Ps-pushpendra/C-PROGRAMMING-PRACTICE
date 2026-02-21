920eda5322589c7ac4fb9a0db86cfada98878abc
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-02-21 15:01:57 +0530

    Update circumference_of_circle.c

--- Basics/circumference_of_circle.c
+++ Basics/circumference_of_circle.c
@@ -1,10 +1,13 @@
+// program to determine the circumference of circle.
+
 #include<stdio.h>
 int main()
 {
  float r,cmf;
  printf("enter radius of circle:");
  scanf("%f",&r);
- cmf=2*3.14159*r;
+ cmf=2*3.14159*r;                 // circumference of circle = 2*pi*radius.
  printf("circumference of circle is %f",cmf);
  return 0;
 }
+
