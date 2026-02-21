ce739b96d13914e5ec962c5fa7b232c7e567b2bf
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-02-21 14:44:13 +0530

    Add files via upload

---
+++ circumference_of_circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+ float r,cmf;
+ printf("enter radius of circle:");
+ scanf("%f",&r);
+ cmf=2*3.14159*r;
+ printf("circumference of circle is %f",cmf);
+ return 0;
+}
