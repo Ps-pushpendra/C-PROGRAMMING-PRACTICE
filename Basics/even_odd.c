29d8e8cb49a321ecd0f784a2c616815890347e89
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-02-24 23:50:14 +0530

    Add files via upload

---
+++ Basics/even_odd.c
@@ -0,0 +1,13 @@
+// program to determine a number is even or odd.
+
+#include<stdio.h>
+int main(){
+int n;
+printf("enter any number:");
+scanf("%d",&n);
+if (n%2==0){
+  printf("entered number is even");
+}
+ else { printf("number is odd"); };
+return 0;
+}
