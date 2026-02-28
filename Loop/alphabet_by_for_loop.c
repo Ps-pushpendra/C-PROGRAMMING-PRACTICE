ba3cb9ed1af70269d91d4f96c9db9e01ba6735b9
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-02-28 20:48:34 +0530

    Update alphabet_by_for_loop.c

--- Loop/alphabet_by_for_loop.c
+++ Loop/alphabet_by_for_loop.c
@@ -1,8 +1,11 @@
+// program to print the alphabet a to z using for loop.
+
 #include<stdio.h>
 int main(){
    char ch='a';
-   for (ch='a';ch<='z';ch++){
+   for (ch='a';ch<='z';ch++){  // same for the uppercase.
     printf("%c ",ch);
    }
    return 0;
+
 }
