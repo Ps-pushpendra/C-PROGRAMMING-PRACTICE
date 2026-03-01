f18b30b7e731ec75d0dd8d802b77b7788be9aae4
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-01 19:01:27 +0530

    Add files via upload

---
+++ Basics/swapping_of_variable_using_temp.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a,b,temp;
+    printf("enter first number(a):");
+    scanf("%d",&a);
+    printf("enter second number(b):");
+    scanf("%d",&b);
+
+    temp=a;
+    a=b;
+    b=temp;
+
+    printf("the values of variable after swapping\n");
+    printf("a=%d\n",a);
+    printf("b=%d\n",b);
+
+    return 0;
+    
+
+}
