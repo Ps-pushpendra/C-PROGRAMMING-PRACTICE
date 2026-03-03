91ec6a2008b29203948a2b9526c2362ce6474133
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-03 22:57:46 +0530

    Add files via upload

---
+++ Loop/calculator_loop.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char choice[10];
+    float n1,n2,result;
+    char operation;
+
+    
+    do{
+
+printf("Enter two integers n1 and n2:");
+scanf("%f%f",&n1,&n2);
+
+printf("press + for ADDITION:\n");
+printf("press - for SUBSTRACTION:\n");
+printf("press * for MULTIPLEICATION:\n");
+printf("press / for DIVISION:\n");
+printf("your choice:");
+scanf(" %c",&operation);
+
+switch(operation) {
+
+case '+':  result=n1+n2;
+	 printf("sum =%0.3f\n",result);
+	 break;
+
+case '-':  result=n1-n2;
+	 printf("subtraction = %0.3f\n",result);
+	 break;
+
+case '*':  result=n1*n2;
+	 printf("multiplication = %0.3f\n",result);
+	 break;
+
+case '/':  result=n1/n2;
+    if (n2==0) printf("not divisible by zero!\n");
+	else printf("division = %0.3f\n",result);
+	 break;
+
+default: printf("invalid input!! please check the input");
+
+ }
+
+      printf("if want to continue then enter yes , if not then no:");
+      scanf("%s",choice);
+
+}while (strcmp(choice,"yes")==0 || strcmp(choice,"YES")==0);
+
+printf("\n#calculator exited#");
+
+}
