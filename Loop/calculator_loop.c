8cc371f8cd1d8b1a501cbd5ef2457babc7e3919b
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-03 23:09:49 +0530

    Update calculator_loop.c

--- Loop/calculator_loop.c
+++ Loop/calculator_loop.c
@@ -1,13 +1,15 @@
-#include<stdio.h>
-#include<string.h>
+// calculator that work until you exit.
 
+
+#include<stdio.h>
+#include<string.h>     // here i use this header file for comparision function of string at bottom.
+                       // because the strings can't be compared directly.
 int main(){
     char choice[10];
     float n1,n2,result;
     char operation;
-
     
-    do{
+ do{
 
 printf("Enter two integers n1 and n2:");
 scanf("%f%f",&n1,&n2);
@@ -20,8 +22,9 @@ printf("your choice:");
 scanf(" %c",&operation);
 
 switch(operation) {
-
-case '+':  result=n1+n2;
+	
+ // can also use numbers(1,2,3..) instesd of signs(+,-,*,/)
+case '+':  result=n1+n2;      
 	 printf("sum =%0.3f\n",result);
 	 break;
 
@@ -36,7 +39,7 @@ case '*':  result=n1*n2;
 case '/':  result=n1/n2;
     if (n2==0) printf("not divisible by zero!\n");
 	else printf("division = %0.3f\n",result);
-	 break;
+	break;
 
 default: printf("invalid input!! please check the input");
 
@@ -48,5 +51,6 @@ default: printf("invalid input!! please check the input");
 }while (strcmp(choice,"yes")==0 || strcmp(choice,"YES")==0);
 
 printf("\n#calculator exited#");
-
+// here this program runs without return 0; statement.
+// because :"In C99 and later standards, reaching the end of main automatically returns 0.”
 }
