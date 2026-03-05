70c01e5bdea9baa98cc36ab69792139c3a9754e8
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-05 20:31:44 +0530

    Update positive_negative_zero.c

--- Conditional/positive_negative_zero.c
+++ Conditional/positive_negative_zero.c
@@ -7,8 +7,8 @@ int n;
 printf("enter a number:");
 scanf("%d",&n);
 
-if (n==0) printf("entered number is zero");
-else if (n>0) printf("positive");
-else printf("negative");
+if (n==0) printf("entered number is zero");  
+else if (n>0) printf("positive");   //number greater than zero
+else printf("negative");            // number smaller than zero
 return 0;
 }
