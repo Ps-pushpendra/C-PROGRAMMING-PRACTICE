04e761e2c7d166f7289b09c23d6ce9eedcbdb496
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-08 19:36:11 +0530

    Update armstrong.c

--- Loop/armstrong.c
+++ Loop/armstrong.c
@@ -1,3 +1,7 @@
+// program to determine the entered number is armstrong number or not
+
+// such as i entered number 153 then if "cube of 1 + cube of 5 + cube of 3"=153 then it is armstrong number.
+
 #include<stdio.h>
 int main(){
     int n,r,call,arm=0;
@@ -5,12 +9,13 @@ int main(){
     scanf("%d",&n);
     call=n;
     while(n>0){
-        r=n%10;
-        arm=(r*r*r)+arm;
-        n=n/10;
+        r=n%10;    // give the last digit
+        arm=(r*r*r)+arm;   // (1*1*1)+(5*5*5)+(3*3*3)
+        n=n/10;    // remove the last digit.
     }
     if (arm==call) printf("armstrong");
     else printf("not armstrong");
 
     return 0;
+
 }
