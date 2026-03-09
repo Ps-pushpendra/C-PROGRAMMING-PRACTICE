8ca04f03697d0778fa31330d5661093786eaed09
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-09 20:26:23 +0530

    Add files via upload

---
+++ Loop/untill_odd.c
@@ -0,0 +1,12 @@
+// program that's not stop until user enters odd number.
+
+#include<stdio.h>
+int main(){
+    int n;
+    do{
+        printf("the execution doesnt stop untill you enter odd number:");
+        scanf("%d",&n);
+    }while(n%2==0);    // loop continue if even.
+    printf("loop breaks !! you entered even number.");
+    return 0;
+}
