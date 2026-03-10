82c6ac58363378fefce3569ea7d2adf34c6c83cf
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-10 19:29:29 +0530

    Update perfect_number.c

--- Loop/perfect_number.c
+++ Loop/perfect_number.c
@@ -3,18 +3,19 @@
   // a number is perfect if it is divisible by its sum of digits.
 
 #include<stdio.h>
-int main(){
+int main(){ 
+  
     int n,sum=0;
 
     printf("enter number:");
     scanf("%d",&n);
 
     for (int i=1;i<n;i++){
-        if (n%i==0) sum+=i;
+        if (n%i==0) sum+=i;  // if condition true then sum of digit.
     }
     if (sum==n) printf("number is perfect");
     else printf("not a perfect number");
 
     return 0;
-
 }
+
