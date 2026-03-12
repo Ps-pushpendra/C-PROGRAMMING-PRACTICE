7abf66c84cf10e0ad1a095a7254e475a9a19d359
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-12 23:46:31 +0530

    Update and rename triangle_right.c to triangle_type.c

--- Conditional/triangle_type.c
+++ Conditional/triangle_type.c
@@ -2,12 +2,17 @@
 
 #include<stdio.h>
 int main(){
-    float a,b,c;       // a,b,c are the sides of triangle.                                         
+    
+    float a,b,c;       // a,b,c are the sides of triangle. 
+    
     printf("enter all three sides of the triangle:");
     scanf("%f%f%f",&a,&b,&c);
+    
     if ((a==b)&&(b==c)) printf("the triangle is equilateral");
     else if ((a==b)||(b==c)||(a==c)) printf("the triangle is isoceles");
     else if (!(a==b)&&!(b==c))printf("the triangle is scalene");
     else printf("the triangle is Right Angled");
+    
     return 0;
+
 }
