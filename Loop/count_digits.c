7b24df9f903d13e4e041aaa4c9420759d6d2f816
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-14 23:41:52 +0530

    Update count_digits.c

--- Loop/count_digits.c
+++ Loop/count_digits.c
@@ -1,3 +1,5 @@
+// program to count digits in entered number
+
 #include<stdio.h>
 int main(){
     int n,count=0;
@@ -6,11 +8,13 @@ int main(){
     scanf("%d",&n);
     printf("digits in %d:",n);
 
-    while(n>0){
+    while(n>0){         // used while loop because number of iteration is not known.
     
         n=n/10;         // if we not assign n here then it will create infinite loop.
-        count++;
+        count++;        // here count increases as opeeration work.
     }
-    printf(" %d",count);
+    
+    printf(" %d",count);  
     return 0;
+    
     }
