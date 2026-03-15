a1ee815683836b2727762f6f8281a9b5287d7e50
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-15 22:33:34 +0530

    Update untill_odd.c

--- Loop/untill_odd.c
+++ Loop/untill_odd.c
@@ -6,7 +6,9 @@ int main(){
     do{
         printf("the execution doesnt stop untill you enter odd number:");
         scanf("%d",&n);
+        
     }while(n%2==0);    // loop continue if even.and if odd entered then loop exited.
+    
     printf("loop breaks !! you entered even number.");
     return 0;
 
