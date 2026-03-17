6006087869cc9e35807439586c01ce9bfecefbb1
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-17 23:42:13 +0530

    Update lcm.c

--- Loop/lcm.c
+++ Loop/lcm.c
@@ -1,11 +1,13 @@
+// program to get LCM of two number
+
 #include<stdio.h>
 int main(){
     int lcm,a,b;
     printf("enter two number for LCM:");
     scanf("%d%d",&a,&b);
 
-    for (int i=(a>b?a:b);;i++){
-        if((i%a==0)&&(i%b==0)){
+    for (int i=(a>b?a:b);;i++){  // ternary operator used. n
+        if((i%a==0)&&(i%b==0)){  // if satisfy both the condition then loop terminates and exited.
          lcm=i; break;
         }
     }
