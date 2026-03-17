e9c8ceadaf21481f2ba198acc59a2c558199d723
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-17 23:31:44 +0530

    Update ternary.c

--- Conditional/ternary.c
+++ Conditional/ternary.c
@@ -1,12 +1,11 @@
-// ternary (conditional statement/operator)
-     // condition? value_if_true : value_if_false ;
+// program to expalin the use of ternary statement.
 
 #include<stdio.h>
 int main(){
 
     int age;
-    printf("enter age:");
-    scanf("%d",&age);
+    printf("enter age:");   // ternary (conditional statement/operator)
+    scanf("%d",&age);       //syntax ---> condition? value_if_true : value_if_false ;
 
      (age>=18)? printf("eligble for vote") : printf("not eligble for vote");
 
