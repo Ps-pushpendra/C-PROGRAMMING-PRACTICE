50100c16cb4aee87b2e95273fbbd41ef84d69401
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-18 23:25:12 +0530

    Update simple_intrest.c

--- Basics/simple_intrest.c
+++ Basics/simple_intrest.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main(){
 float p,r,t,si;
+  
 printf("enter principal, rate,time:");
 scanf("%f%f%f",&p,&r,&t);
-si=(p*r*t)/100;
+  
+si=(p*r*t)/100; // si formula
 printf("simple intrest:%f",si);
+  
 return 0;
 }
