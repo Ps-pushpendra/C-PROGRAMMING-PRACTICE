535f1f3886cdef3c03bfedc06f71fb39187ccdea
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-19 23:04:20 +0530

    Update neon.c

--- Loop/neon.c
+++ Loop/neon.c
@@ -1,7 +1,7 @@
  // program to check a number is neon or not.
  
- // A number is neon if the sum of digits of its square equals the number itself.
-
+ // A number is neon if the sum of digits of its square equals the number itself. eg.9 is neon number
+  
 
 #include<stdio.h>
 int main(){
@@ -10,12 +10,13 @@ int main(){
     printf("enter number:");
     scanf("%d",&n);
 
-    sqr = n*n;
+    sqr = n*n;  // we can also use here math.h library for square.
 
     while(sqr>0){
-        r=sqr%10;
+     
+        r=sqr%10;  // last digit of square
         sum=sum+r;
-        sqr=sqr/10;
+        sqr=sqr/10; // remove the last digit.
     }
 
    if(sum==n) printf("number is NEON");
