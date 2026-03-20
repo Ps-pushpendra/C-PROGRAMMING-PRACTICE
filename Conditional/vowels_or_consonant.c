3f3d0f0b53157b5f3c6f6c803f70058508c4b82d
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-20 22:22:00 +0530

    Update vowels_or_consonant.c

--- Conditional/vowels_or_consonant.c
+++ Conditional/vowels_or_consonant.c
@@ -1,3 +1,5 @@
+// program to check the entered character is vowel or consonant.
+
 #include<stdio.h>
 int main(){
     char ch;
@@ -5,7 +7,7 @@ int main(){
     printf("enter any character:");
     scanf("%c",&ch);
 
-    if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u')) printf("vowels");
+    if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u')) printf("vowels");  // ensure of assignment operator and upper or lower case of character.
 
     else if((ch=='A')||(ch=='E')||(ch=='I')||(ch=='O')||(ch=='U')) printf("VOWELS");
 
@@ -13,7 +15,7 @@ int main(){
 
     else if ('A'<=ch && ch<='Z') printf("CONSONANT");
 
-    else printf("invalid input!!");
+    else printf("invalid input!!"); // if number or symbols the invalid.
     
     return 0;
 }
