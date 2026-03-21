3fb2cb8e76d9799278e908c2fc7710f55d4adbec
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-21 16:08:58 +0530

    Update reverse_digits.c

--- Loop/reverse_digits.c
+++ Loop/reverse_digits.c
@@ -1,14 +1,20 @@
+// program to reverse the digits of a number.
+
+// for example : 251 ---> 152.
+
 #include<stdio.h>
 int main(){
+    
     int n,r,rev=0;
     printf("enter:");
     scanf("%d",&n);
 
-    while(n!=0){
+    while(n!=0){  // for better understanding use dry run using copy&pen.
        r=n%10;
        rev= (rev*10)+r;
        n=n/10;
     }
+    
    printf("%d",rev);
 
   return 0;
