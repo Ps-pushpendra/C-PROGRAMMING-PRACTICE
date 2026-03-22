317a9f842c4c0fd7c2cedff93e7da9cdb622eafa
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-22 23:04:56 +0530

    Update data_store_in_array.c

--- data_store_in_array.c
+++ data_store_in_array.c
@@ -1,14 +1,16 @@
+// program to store data in array.
+
 #include<stdio.h>
 int main(){
-   int marks[6];
+   int marks[6];  // initialization of array with size.
 
    for (int i=0;i<=5;i++){
-    printf("enter integer data:");
+    printf("enter integer data:");   // taking value to store it.
     scanf("%d",&marks[i]);
    }
 
    for(int i=0;i<=5;i++){
-    printf("at index %d value stored = %d \n",i,marks[i]);
+    printf("at index %d value stored = %d \n",i,marks[i]); // printing the value with index.
    }
    return 0;
 }
