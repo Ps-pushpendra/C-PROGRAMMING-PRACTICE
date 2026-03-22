c112cf541102743cbe9791a965e73d085b96c9a2
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-22 23:10:53 +0530

    Update min_max_in_array.c

--- Array/min_max_in_array.c
+++ Array/min_max_in_array.c
@@ -1,26 +1,28 @@
+// program to find the minimun & maximum value in the array.
+
 #include<stdio.h>
 int main(){
     int array[30],n,i;
     int min,max,min_idx,max_idx;
 
-    printf("enter no. of element in array:");
+    printf("enter no. of element in array:");   
     scanf("%d",&n);
     printf("the number of element in array: %d \n",n);
-    printf("\n");
+    printf("\n");   // for space to better view
 
     for(i=0;i<n;i++){
-        printf("enter elements:");
+        printf("enter elements:");   // taking elements.
         scanf("%d",&array[i]);
     }
 
-    min = array[0];
-    max = array[0];
-    min_idx = 0;
+    min = array[0];   // initialize first as min at index 0 for using it  on loop.
+    max = array[0];    // similarly for max
+    min_idx = 0;       //also for index
     max_idx = 0;
 
 
     for(i=0;i<n;i++){
-        if(array[i]<min) {
+        if(array[i]<min) {  // do dry run for better understanding.
             min = array[i];
             min_idx = i;
         }
