085ddb49b5011c673affd32f11084b4a2c35272c
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-24 23:39:05 +0530

    Update element_search_array.c

--- Array/element_search_array.c
+++ Array/element_search_array.c
@@ -1,16 +1,25 @@
+// program to serach the element in array
+
 #include<stdio.h>
 int main(){
+    
     int n,array[30],key,found=0;
+
+    // no. of input
     printf("the no. of element: ");
     scanf("%d",&n);
-
+    
+    // taking elements
     for(int i=0;i<n;i++){
-    printf("enter elements: ");
-    scanf("%d",&array[i]);}
+         printf("enter elements: ");
+         scanf("%d",&array[i]);
+    }
 
+    // taking element for search
     printf("enter search element: ");
     scanf("%d",&key);
 
+    // searching the element if it is in array
     for(int i=0;i<n;i++){
         if(key==array[i]) {
             printf("element found at index %d",i);
@@ -18,7 +27,10 @@ int main(){
             break;    
         }
     }
-    if (found == 0){ printf("element not in the array");
+
+    // if not
+    if (found == 0){
+        printf("element not in the array");
         printf("not found!!");
     }
 
