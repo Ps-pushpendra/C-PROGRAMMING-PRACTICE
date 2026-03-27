6d6dc23b9287eceb8c62a5d4a57fab221b76e697
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-28 00:23:51 +0530

    Add files via upload

---
+++ Loop/count _using_while_loop.c
@@ -0,0 +1,17 @@
+// program to count number using while loop upto user's choice.
+
+#include<stdio.h>
+int main(){
+  
+    int n;
+    printf("enter number where you want to end:");
+    scanf("%d",&n);
+
+    int i;
+    while(i<=n){
+        printf("%d \n",n);    // print the current value of n untill the condition match.
+        i++;
+    }
+return 0;    
+
+}
