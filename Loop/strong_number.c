fe8d218d61d2bc612f92863277ac4fee233bb4ad
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-29 13:55:37 +0530

    Update strong_number.c

--- Loop/strong_number.c
+++ Loop/strong_number.c
@@ -1,19 +1,27 @@
+// program to check a number is strong or not.
+
+// strong number --> the factorial of its digits and sum of them is equal to that number.(e.g. 145)
+
 #include<stdio.h>
 int main(){
+    
     int n,sum=0,r,fact=1;
 
     printf("enter number:");
     scanf("%d",&n);
-    int temp=n;
+    
+    int temp=n;   // storing in temperory variable to call it later
 
     while(temp!=0){
-        r=temp%10;
-        for(int i=1;i<=r;i++){
+        
+        r=temp%10;    // last digit
+        
+        for(int i=1;i<=r;i++){  //factorial of last digit
             fact=fact*i;
         }
-        sum+=fact;
-        temp/=10;
-        fact=1;
+        sum+=fact;     // adding the factorial if the digits
+        temp/=10;      // removing the last digit
+        fact=1;        // intitalizing the fact to 1 becoz. we have to find the factorial of other remaining digits
     }
     if (sum==n){
         printf("STRONG NUMBER");
