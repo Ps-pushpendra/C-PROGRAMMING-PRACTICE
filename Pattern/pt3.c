16829d283323cce50ba8c9e1c8f7940fec84f487
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-03-31 16:46:10 +0530

    Update pt3.c

--- Pattern/pt3.c
+++ Pattern/pt3.c
@@ -1,14 +1,24 @@
+// program to print the pattern
+
+/*
+
+1 2 3 4 
+1 2 3 4
+1 2 3 4
+1 2 3 4
+
+*/
+
+
 #include<stdio.h>
 int main(){
   int n=4;
+  
   for(int i=1;i<=n;i++){
    for(int j=1;j<=n;j++){
       printf("%d ",j);
-    
     }
     printf("\n");
-
   }
-  
     return 0;
 }
