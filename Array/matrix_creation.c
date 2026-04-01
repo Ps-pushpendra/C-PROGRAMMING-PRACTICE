c50b8926213d26583819ffa4010621a7b7595478
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-04-01 23:10:08 +0530

    Update matrix_creation.c

--- Array/matrix_creation.c
+++ Array/matrix_creation.c
@@ -1,18 +1,24 @@
+// program to create the matrics and display it.
+
 #include<stdio.h>
 int main(){
+    
     int arr[5][5],r,c;
    
-    printf("enter no. of rows & column: ");
+    printf("enter no. of rows & column: ");  // taking the rows and column of matrix
     scanf("%d%d",&r,&c);
-     printf("enter 9 elements for matrix A:\n");
-    printf("enter the elements:\n");
-
+    
+    printf("for MATRIX A \n enter the elements:\n");
+    
+    // storing elements in array(matrics)
     for (int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
+        for(int j=0;j<c;j++){      
            scanf("%d",&arr[i][j]);
         }
     }
+    
     printf("\n");
+    // printing the matirx
     printf("the matrix A:\n");
 
      for (int i=0;i<r;i++){
