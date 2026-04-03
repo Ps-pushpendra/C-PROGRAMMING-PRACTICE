89339a65243a068fe3e984a184f91e1198aed05b
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-04-03 16:52:38 +0530

    Update matrix_transpose.c

--- Array/matrix_transpose.c
+++ Array/matrix_transpose.c
@@ -1,13 +1,17 @@
+// program to find the transpose of a matrix.
+
 #include<stdio.h>
 int main(){
     int arr[3][3],i,j;
 
+    // creation(input)
      for(i=0;i<3;i++){
         for(j=0;j<3;j++){
         printf("enter the element:");
         scanf("%d",&arr[i][j]);
         }
      }
+     // displaying the matrix
      printf("\n");
      printf("the matrix is :");
      printf("\n");
@@ -18,16 +22,19 @@ int main(){
         }
         printf("\n");
      }
+
+    // displaying the transpose 
      printf("\n");
      printf("the TRANSPOSE of matrix :");
      printf("\n");
 
      for(i=0;i<3;i++){
         for(j=0;j<3;j++){
-        printf(" %d ",arr[j][i]);
+        printf(" %d ",arr[j][i]);  // just interchange i & j form upper loop
         }
         printf("\n");
      }
+    
      return 0;
 
     }
