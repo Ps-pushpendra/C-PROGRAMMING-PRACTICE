751e44f743d7559d5c8f6dabcf5d79131a0a91dc
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-04-04 23:00:02 +0530

    Update string_len.c

--- Array/string_len.c
+++ Array/string_len.c
@@ -1,13 +1,18 @@
+// program to determine the length of the string
+
 #include<stdio.h>
 #include<string.h>
 
 int main(){
+    
     char name[20];
-    printf("enter your name: ");
+    
+    printf("enter your name: ");    // input string
     gets(name);
 
     printf("your name is %s",name);
-    int len=strlen(name);
+    
+    int len = strlen(name);   // using inbuilt function for determining the length
     printf("\n the length of the name is %d",len);
 
     return 0;
