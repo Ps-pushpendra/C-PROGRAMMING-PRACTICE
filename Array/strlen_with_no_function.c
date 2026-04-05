79a8f10bf471c35c3d0266c812e390813ac262b5
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-04-05 21:05:27 +0530

    Update strlen_with_no_function.c

--- Array/strlen_with_no_function.c
+++ Array/strlen_with_no_function.c
@@ -1,13 +1,17 @@
+// program to find the length of string without length function.
+
 #include<stdio.h>
-#include<string.h>
 int main(){
     char str[20];
     int count=0;
+    
     printf("enter string:");
-    gets(str);
+    gets(str); // here gets function can read string also with space 
+    
     while(str[count]!='0'){
         count++;
-    }
+    } 
+    
     printf("length of string: %d",count);     
     return 0;
 }
