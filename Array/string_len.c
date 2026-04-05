2ac826e23846d726b5e5d6b1a8f8734d4044649e
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-04-05 21:07:31 +0530

    Update string_len.c

--- Array/string_len.c
+++ Array/string_len.c
@@ -8,11 +8,11 @@ int main(){
     char name[20];
     
     printf("enter your name: ");    // input string
-    gets(name);
+    gets(name);     // here gets function can read string also with space 
 
     printf("your name is %s",name);
     
-    int len = strlen(name);   // using inbuilt function for determining the length
+    int len = strlen(name);   // using inbuilt function for determining the length and storing it in len variable
     printf("\n the length of the name is %d",len);
 
     return 0;
