7f3c7901e374e9b0444ff2e4581931b2cb9d7b52
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-04-09 22:14:19 +0530

    Update string_revesre.c

--- Array/string_revesre.c
+++ Array/string_revesre.c
@@ -1,15 +1,18 @@
+// program to reverse the string.
+
 #include<stdio.h>
 int main(){
     char str[20],str2[20],i=0;
     int count=0;
+    // input string
     printf("enter string:");
     gets(str);
-
+    
     while(str[count]!='\0'){
         count++;
     }
     
-    count = count-1;
+    count = count-1;  // decreasing due to indexing.
 
     while(count>=0){
         str2[i] = str[count];
@@ -17,7 +20,7 @@ int main(){
         count--;
     }
 
-    str2[i] = '\0';
+    str2[i] = '\0'; // adding the null character at the end here i=len of string.
     printf("original string: %s \n",str);
     printf("reverse string: %s",str2);
     return 0;
