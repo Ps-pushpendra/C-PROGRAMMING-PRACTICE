a3f301a25bb2a999e529de1f09a89bef6088f3ae
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-04-10 23:31:54 +0530

    Add files via upload

---
+++ Array/elements_ascending.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+    int a[20];
+    int n,j,i,t=0;
+
+    printf("enter no. of element:");
+    scanf("%d",&n);
+
+    printf("enter %d element for array:",n);
+    printf("\n");
+
+    for(i=0;i<n;i++){
+        printf("enter element:");
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        printf(" %d ",a[i]);
+    }
+    for(i=0;i<n-1;i++){
+        for(j=i+1;j<n;j++){
+            if(a[i]>a[j]){
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+    }
+    printf("\n");
+    printf("arranged elements: ");
+    for(i=0;i<n;i++){
+        printf(" %d ",a[i]);
+    }
+    return 0;
+}
