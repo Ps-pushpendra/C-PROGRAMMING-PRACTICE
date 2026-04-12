0ba7c20efcaf9b6957da253f4bb7d90dc7104f7b
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-04-12 23:06:45 +0530

    Update string_copy.c

--- Array/string_copy.c
+++ Array/string_copy.c
@@ -1,12 +1,17 @@
+// program to copy the string to another variable.
+
 #include<stdio.h>
 #include<string.h>
+
 int main(){
-    char str1[10],str2[10];
+    char str1[10],str2[10];// creates two array one for storing string and one for copy it
     printf("enter string:");
-    gets(str1);
+    gets(str1); // use the full string also with space
+    
     strcpy(str2,str1);
     printf("%s",str1);
     printf("\n%s",str2);
+    
     return 0;
     
 }
