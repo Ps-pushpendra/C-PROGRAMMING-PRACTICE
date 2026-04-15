8280789fe5ee71e169adcf59e6e1bde9abefec4e
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-04-15 22:51:04 +0530

    Add files via upload

---
+++ Loop/sum_of_first_n_natural_number.c
@@ -0,0 +1,13 @@
+// program to print the sum of first n natural number.
+
+#include<stdio.h>
+int main(){
+    int n,sum=0,i;
+    printf("enter number:");
+    scanf("%d",&n);
+    for (i=1;i<=n;i++){
+        sum = sum+i ; // sum+=i
+    }
+   printf("sum of first %d natural number: %d \n",n,sum);
+   return 0; 
+}
