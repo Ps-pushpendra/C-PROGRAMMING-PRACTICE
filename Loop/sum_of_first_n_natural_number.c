c9dbc9574b2761b953bebe41dadf89669a45db6f
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-04-15 22:52:20 +0530

    Update sum_of_first_n_natural_number.c

--- Loop/sum_of_first_n_natural_number.c
+++ Loop/sum_of_first_n_natural_number.c
@@ -5,8 +5,10 @@ int main(){
     int n,sum=0,i;
     printf("enter number:");
     scanf("%d",&n);
+    
     for (i=1;i<=n;i++){
         sum = sum+i ; // sum+=i
+        
     }
    printf("sum of first %d natural number: %d \n",n,sum);
    return 0; 
