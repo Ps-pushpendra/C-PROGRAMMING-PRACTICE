55f47fcc62c0ef8ebe290ef510e4f9379c6064b5
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-04-16 22:52:42 +0530

    Update armstrong.c

--- Loop/armstrong.c
+++ Loop/armstrong.c
@@ -3,3 +3,34 @@
 // such as i entered number 153 then according to no.of digits i.e. 3 "cube of 1 + cube of 5 + cube of 3"=153 then it is armstrong number.
            // if i = 1645 then " 1 power 4 + 6 power 4 + 4 power 4 + 5 power 4 " = i then it is armstrong number.
 
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n,arm=0,r,i,len=0;
+    printf("enter number:");
+    scanf("%d",&n);
+    int call = n;
+    
+while(n!=0){
+    r=n%10;
+    n=n/10;
+    len++;
+}    
+printf("length = %d",len);
+printf("\n");
+
+ // Reset n before second loop
+    n = call;
+
+while(n!=0){
+    r=n%10;
+    arm = pow(r,len) + arm ;
+    n=n/10;
+}
+
+if (arm == call) printf("ARMSTRONG NUMBER");
+else printf("NOT ARMSTRONG NUMBER");
+    
+    return 0;
+}
