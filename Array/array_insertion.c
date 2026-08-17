098c6d91b6f53d3b7ddd842951521dae7f559aa7
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-08-17 14:06:53 +0530

    Update array_insertion.c

--- Array/array_insertion.c
+++ Array/array_insertion.c
@@ -1,19 +1,47 @@
-#include<stdio.h>
-int main(){
-    int arr[5]= {10,20,40,50};
-    int n = sizeof(arr)/sizeof(arr[0]);
+// program to insert an element in array.
+#include <stdio.h>
 
-    int value = 30;
-    int pos = 3;
+int main() {
+   
+   int a[500] ,n,i,j,e,p;
 
-    for(int i = n-1;i>=pos-1;i--){
-           arr[i+1]=arr[i];
+    printf("enter number of element:");
+    scanf("%d",&n);
+
+   printf("enter element to insert:");
+   scanf("%d",&e); 
+    
+    printf("enter position to insert:");
+    scanf("%d",&p);
+
+    printf("enter element:\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
     }
-    arr[pos-1]=value;
-    n++;
+    printf("\n");
 
-    for(int i=0;i<n;i++){
-        printf("%d ",arr[i]);
+    printf("old array: ");
+
+      for(i=0;i<n;i++){
+        printf("%d ",a[i]);
     }
+
+
+      printf("\n");
+
+      for(i=n-1;i>=p-1;i--){
+        a[i+1] = a[i];
+    }
+    
+    a[p-1] = e;
+
+    
+    printf("inserted array: ");
+
+      for(i=0;i<=n;i++){
+        printf("%d ",a[i]);
+    }
+
+
     return 0;
 }
